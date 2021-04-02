7c559da931a73781ee1c887b6240a72f88747aaa
mtsukuda <mats.tsukuda@gmail.com>
2021-04-02 17:09:48 +0900

    fix wrong path: hello -> echo.

--- src/functions/echo/index.ts
+++ src/functions/echo/index.ts
@@ -6,7 +6,7 @@ export default {
     {
       http: {
         method: 'post',
-        path: 'hello',
+        path: 'echo',
         request: {
           schema: {
             'application/json': schema
