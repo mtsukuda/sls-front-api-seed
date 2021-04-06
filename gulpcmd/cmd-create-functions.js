9a5ce54ca791f9a8d9e5ccd63ecefc2e39945bc0
mtsukuda <mats.tsukuda@gmail.com>
2021-04-06 17:38:14 +0900

    implement gulp function: create-functions.

--- gulpcmd/cmd-create-functions.js
+++ gulpcmd/cmd-create-functions.js
@@ -2,12 +2,16 @@ const _ = require('lodash');
 const gulp = require('gulp');
 const chalk = require('chalk');
 const gulpfs = require('../gulplib/gulpfs');
+const FRONT_API_FUNCTIONS_CONFIG_JSON_PATH = '../seed/functions/config.json';
 
 /**
  * Create Functions
  */
 gulp.task('create-functions', function (done){
   console.log(' 🚀🚀🚀 ' + chalk.bgBlue(' create-functions ') + ' 🚀🚀🚀 ');
+  if(gulpfs.fileExists(FRONT_API_FUNCTIONS_CONFIG_JSON_PATH) === false) {
+    throw new Error(`Could not find ${FRONT_API_FUNCTIONS_CONFIG_JSON_PATH}.`);
+  }
   done();
 });
 
