e0c419843e6f5dab138e56ebf64632d66ca03c7b
mtsukuda <mats.tsukuda@gmail.com>
2021-04-23 08:54:20 +0900

    refactor: prettier sorce code.

--- gulplib/gulpfs.js
+++ gulplib/gulpfs.js
@@ -1,24 +1,29 @@
-const _ = require('lodash');
-const FS = require('fs');
+const _ = require("lodash");
+const FS = require("fs");
 
 module.exports.jsonFilePaths = (dirPath) => {
   let allFiles = FS.readdirSync(dirPath);
   if (allFiles && _.isArray(allFiles)) {
     let jsonFilePathList = allFiles.filter(function (filePath) {
-      return FS.statSync(`${dirPath}/${filePath}`).isFile() && /.*\.json$/.test(filePath);
+      return (
+        FS.statSync(`${dirPath}/${filePath}`).isFile() &&
+        /.*\.json$/.test(filePath)
+      );
     });
-    jsonFilePathList = jsonFilePathList.map(filePath => `${dirPath}/${filePath}`);
+    jsonFilePathList = jsonFilePathList.map(
+      (filePath) => `${dirPath}/${filePath}`
+    );
     return jsonFilePathList;
   }
   return null;
-}
+};
 
 module.exports.cleanDirectories = (targetPath) => {
   module.exports.deleteDirectoryRecursive(targetPath);
   FS.mkdirSync(targetPath, (err) => {
     throw err;
   });
-}
+};
 
 module.exports.writeDistFile = (distFilePath, buffer) => {
   try {
@@ -27,7 +32,7 @@ module.exports.writeDistFile = (distFilePath, buffer) => {
   } catch (err) {
     throw err;
   }
-}
+};
 
 module.exports.appendDistFile = (distFilePath, buffer) => {
   try {
@@ -36,38 +41,40 @@ module.exports.appendDistFile = (distFilePath, buffer) => {
   } catch (err) {
     throw err;
   }
-}
+};
 
 module.exports.deleteDirectoryRecursive = (path) => {
-  if(FS.existsSync(path)) {
-    FS.readdirSync(path).forEach(function(file) {
+  if (FS.existsSync(path)) {
+    FS.readdirSync(path).forEach(function (file) {
       let curPath = path + "/" + file;
-      if(FS.lstatSync(curPath).isDirectory()) { // recurse
+      if (FS.lstatSync(curPath).isDirectory()) {
+        // recurse
         module.exports.deleteDirectoryRecursive(curPath);
-      } else { // delete file
+      } else {
+        // delete file
         FS.unlinkSync(curPath);
       }
     });
     FS.rmdirSync(path);
   }
-}
+};
 
 module.exports.readWholeFile = (targetPath) => {
   try {
-    return FS.readFileSync(targetPath, 'utf8');
+    return FS.readFileSync(targetPath, "utf8");
   } catch (err) {
-    if (err.code === 'ENOENT') {
+    if (err.code === "ENOENT") {
       console.log(`Could not find: "${targetPath}"`);
       return null;
     }
     console.log(err);
     return null;
   }
-}
+};
 
-module.exports.JSONdata = (filePath, log=true) => {
+module.exports.JSONdata = (filePath, log = true) => {
   let fileBuffer = module.exports.readWholeFile(filePath);
-  if (fileBuffer === null) return '';
+  if (fileBuffer === null) return "";
   let jsonData = JSON.parse(fileBuffer);
   if (log) console.log(jsonData);
   return jsonData;
